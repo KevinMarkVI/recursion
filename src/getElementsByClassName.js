f7a41c60eeddfd5308728c6376a1058fe669e475
Kevin <kevinmarkvi@gmail.com>
2015-02-21 13:28:45 -0800

    Stringify: MOSTLY COMPLETE, Elements: Complete

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+	var results = [];
+	var getElementsByClassNameHelper = function(element, className) {
+		if (element.classList === undefined) {
+			return results;
+		} else {
+			for (var j = 0; j < element.classList.length; j++) {
+				if (element.classList[j] === className) {
+					results.push(element);
+				}
+			}
+		}
+		for (var i = 0; i < element.childNodes.length; i++) {
+			getElementsByClassNameHelper(element.childNodes[i], className);
+		}
+	};
+	getElementsByClassNameHelper(document.body, className);
+	return results;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,94 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+	var str;
+  if (obj === null) {
+    return "null";
+  }
+  if (obj === undefined) {
+    return undefined;
+  }
+  if (obj === true) {
+    return "true";
+  }
+  if (obj === false) {
+    return "false";
+  }
+  if (typeof obj !== "object"){
+    str = "";
+    if (typeof obj === "number") {
+      return obj.toString();
+    } else {
+      str = "\"" + obj + "\"";
+      return str;
+    }
+  }
+  if (Array.isArray(obj)) {
+    str = "[" ;
+    for (var i = 0; i < obj.length; i++ ) {
+      if (obj[i] === obj[obj.length - 1]) {
+        str += stringifyJSON(obj[i]);
+      } else {
+        str += stringifyJSON(obj[i]) + ",";
+      }
+    }
+    str += "]";
+    return str;
+  } else {
+    str = "{";
+    if (Object.keys(obj).length === 0) {
+        return "{}";
+    }
+    for (var j in obj) {
+      var keys = Object.keys(obj);
+      if (j === keys[keys.length -1]) {
+        str += stringifyJSON(j) + ":" + stringifyJSON(obj[j]);
+      } else {
+        str += stringifyJSON(j) + ":" + stringifyJSON(obj[j]) + ",";
+      }
+    }
+    str += "}";
+    return str;
+  }
 };
+
+var stringifiableObjects = [
+  9,
+  null,
+  true,
+  false,
+  "Hello world",
+  [],
+  [8],
+  ["hi"],
+  [8, "hi"],
+  [1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999],
+  [8, [[],3,4]],
+  [[[["foo"]]]],
+  {},
+  {"a": "apple"},
+  {"foo": true, "bar": false, "baz": null},
+  {"boolean, true": true, "boolean, false": false, "null": null },
+  // basic nesting
+  {"a":{"b":"c"}},
+  {"a":["b", "c"]},
+  [{"a":"b"}, {"c":"d"}],
+  {"a":[],"c": {}, "b": true}
+];
+
+for (var j = 0; j < stringifiableObjects.length; j += 1) {
+  var truthy = stringifyJSON(stringifiableObjects[j]) == JSON.stringify(stringifiableObjects[j]);
+  if (!truthy) {
+    console.log("FAILING ON", stringifiableObjects[j]);
+    console.log(
+      "\t",
+      "INPUT:",
+      stringifiableObjects[j],
+      "OUTPUT:", stringifyJSON(stringifiableObjects[j]),
+      "EXPECTED:", JSON.stringify(stringifiableObjects[j])
+    );
+  }
+  
+
+}
+
